950207fe247174c3223e946129ab60c59457f27e
Andrei Dorneles Cruz <andrei.cruz@ibm.com>
2019-04-05 11:18:26 -0300

    metodos alterados

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,7 +9,10 @@ class ArticlesController < ApplicationController
 
     def new
         @article = Article.new
+    end
 
+    def edit
+        @article = Article.find(params[:id])
     end
 
     def create
@@ -21,7 +24,24 @@ class ArticlesController < ApplicationController
             render 'new'
         end
     end
+
+    def update
+        @article = Article.find(params[:id])
        
+        if @article.update(article_params)
+          redirect_to @article
+        else
+          render 'edit'
+        end
+    end
+       
+    def destroy
+        @article.Article.find(params[:id])
+        @article.destroy
+
+        redirect_to articles_path
+    end
+
     private
     def article_params
         params.require(:article).permit(:title, :text)
